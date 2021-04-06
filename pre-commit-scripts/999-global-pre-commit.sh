a05fffd824f177d85c59487c616d6f44352f1a38
Christophe Avonture <cavo789@yahoo.com>
2021-04-06 23:16:58 +0200

    chore: rename files / folder

--- pre-commit-scripts/999-global-pre-commit.sh
+++ pre-commit-scripts/999-global-pre-commit.sh
@@ -6,9 +6,9 @@
 #
 # Install:
 # cd path/to/git/repo
-# mkdir -p .git/hooks/pre-commit
-# curl -fL -o .git/hooks/pre-commit/999-global-pre-commit.sh https://raw.githubusercontent.com/cavo789/bash_git_hooks/main/pre-commit/999-global-pre-commit.sh
-# chmod +x .git/hooks/pre-commit/999-global-pre-commit.sh
+# mkdir -p .git/hooks/pre-commit-scripts
+# curl -fL -o .git/hooks/pre-commit-scripts/999-global-pre-commit.sh https://raw.githubusercontent.com/cavo789/bash_git_hooks/main/pre-commit/999-global-pre-commit.sh
+# chmod +x .git/hooks/pre-commit-scripts/999-global-pre-commit.sh
 
 if [[ -f ".git/hooks/pre-commit" ]]; then
   type realpath >/dev/null 2>&1 || { echo >&2 "NOTE: the realpath binary is required to chain to the repo-specific pre-commit hook. Ignoring."; exit 0; }

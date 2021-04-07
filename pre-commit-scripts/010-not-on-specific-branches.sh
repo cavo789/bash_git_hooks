301558c0ae71b003ed44331358ac8a51ad11a496
Christophe Avonture <christophe.avonture@bosa.fgov.be>
2021-04-07 11:21:47 +0200

    chore: small changes, shellcheck corrections

--- pre-commit-scripts/010-not-on-specific-branches.sh
+++ pre-commit-scripts/010-not-on-specific-branches.sh
@@ -6,13 +6,13 @@
 #
 # Please read https://github.com/cavo789/bash_git_hooks#install for installation guide
 
-BRANCH=`git rev-parse --abbrev-ref HEAD`
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
 # Don't allow to commit directly on these branches
 if [[ "$BRANCH" == "main" || "$BRANCH" == "master" || "$BRANCH" == "dev" ]]; then
-  echo "You are on branch $BRANCH. Are you sure you want to commit to this branch?"
-  echo "If so, commit with the --no-verify parameter to bypass this pre-commit hook."
-  exit 1
+    echo "You are on branch $BRANCH. Are you sure you want to commit to this branch?"
+    echo "If so, commit with the --no-verify parameter to bypass this pre-commit hook."
+    exit 1
 fi
 
 exit 0
--- pre-commit-scripts/999-global-pre-commit.sh
+++ pre-commit-scripts/999-global-pre-commit.sh
@@ -6,12 +6,15 @@
 #
 # Please read https://github.com/cavo789/bash_git_hooks#install for installation guide
 
-if [[ -f ".git/hooks/pre-commit" ]]; then
-  type realpath >/dev/null 2>&1 || { echo >&2 "NOTE: the realpath binary is required to chain to the repo-specific pre-commit hook. Ignoring."; exit 0; }
-  if [[ "$(realpath "${BASH_SOURCE[0]}")" != "$(realpath ".git/hooks/pre-commit")" ]]; then
-    .git/hooks/pre-commit
-    exit $?
-  fi
+if [[ -f ".git/hooks/pre-commit-scripts" ]]; then
+    type realpath >/dev/null 2>&1 || {
+        echo >&2 "NOTE: the realpath binary is required to chain to the repo-specific pre-commit hook. Ignoring."
+        exit 0
+    }
+    if [[ "$(realpath "${BASH_SOURCE[0]}")" != "$(realpath ".git/hooks/pre-commit-scripts")" ]]; then
+        .git/hooks/pre-commit-scripts
+        exit $?
+    fi
 fi
 
 exit 0

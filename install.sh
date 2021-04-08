1ec55d1c701e6a4b314b74871e6baf47c05214e3
Christophe Avonture <christophe.avonture@bosa.fgov.be>
2021-04-08 08:28:59 +0200

    chore: refactor install script

--- install.sh
+++ install.sh
@@ -2,8 +2,58 @@
 
 # Base URL of our git bash hooks repo
 REMOTE="https://raw.githubusercontent.com/cavo789/bash_git_hooks/main"
+# Local folder where scripts will be stored
+LOCAL=""
 
-function installPreCommit () {
+# region-docblock
+# Private function, loop an array with filenames and download these
+# files locally
+# endregion
+function installHook() {
+    hookName="$1" # The hook name like "pre-commit"
+
+    # Get the pre-commit hook
+    printf "\n\e[1;33m%s\e[m" "Download the $hookName hook"
+    printf "\n\e[1;34m%s\e[m" "    Get the $hookName bash script"
+
+    # Create the local folder
+    mkdir -p "$LOCAL"
+
+    curl -fL -o "$LOCAL/$hookName" "$REMOTE/$hookName" --no-progress-meter
+    chmod +x "$LOCAL/$hookName"
+}
+
+# region-docblock
+# Private function, loop an array with filenames and download these
+# files locally
+# endregion
+function installScripts() {
+    hookName="$1"   # The hook name like "pre-commit"
+    arrScripts="$2" # An array with the list of scripts to download
+
+    # Scripts will be stored in a "xxx-scripts" folder: all scripts for the "pre-commit"
+    # hook will be in the "pre-commit-scripts" folder. Same logic for the remote URL
+    local="$LOCAL/$hookName-scripts"
+    remote="$REMOTE/$hookName-scripts"
+
+    printf "\n\e[1;34m%s\e[m" "    Download $hookName scripts"
+
+    # Create the local folder
+    mkdir -p "$local"
+
+    # Download scripts
+    for script in "${arrScripts[@]}"; do
+        printf "\n\e[1;36m%s\e[m" "        Download $script"
+
+        curl -fL -o "$local/$script" "$remote/$script" --no-progress-meter
+        chmod +x "$local/$script"
+    done
+}
+
+# region-docblock
+# Install the pre-commit hooks and its multiple scripts
+# endregion
+function installPreCommit() {
     # Declare an array with the list of scripts present in the repository
     declare -a arrScripts=(
         "010-not-on-specific-branches.sh"
@@ -12,63 +62,22 @@ function installPreCommit () {
         "999-global-pre-commit.sh"
     )
 
-    # Get the pre-commit hook
-    printf "\n\e[1;33m%s\e[m" "Download the pre-commit hook"
-
-    printf "\n\e[1;34m%s\e[m" "    Get the pre-commit bash script"
-
-    curl -fL -o "$LOCAL/pre-commit" "$REMOTE/pre-commit" --no-progress-meter
-    chmod +x "$LOCAL/pre-commit"
-
-    # Now, download each scripts from the REMOTE to the LOCAL folder
-    LOCAL_SCRIPTS="$LOCAL/pre-commit-scripts"
-    REMOTE_SCRIPTS="$REMOTE/pre-commit-scripts"
-
-    # Create the local folder
-    mkdir -p "$LOCAL_SCRIPTS"
-
-    printf "\n\e[1;34m%s\e[m" "    Download pre-commit scripts"
-
-    # Download scripts
-    for script in "${arrScripts[@]}"; do
-        printf "\n\e[1;36m%s\e[m" "        Download $script"
-
-        curl -fL -o "$LOCAL_SCRIPTS/$script" "$REMOTE_SCRIPTS/$script" --no-progress-meter
-        chmod +x "$LOCAL_SCRIPTS/$script"
-    done
+    installHook "pre-commit"
+    installScripts "pre-commit" "${arrScripts[@]}"
 }
 
-function installCommitMsg () {
+# region-docblock
+# Install the commit-msg hooks and its multiple scripts
+# endregion
+function installCommitMsg() {
     # Declare an array with the list of scripts present in the repository
     declare -a arrScripts=(
         "010-wip-skip-ci.sh"
         "020-check-prefix.sh"
     )
 
-    # Get the commit-msg hook
-    printf "\n\e[1;33m%s\e[m" "Download the commit-msg hook"
-
-    printf "\n\e[1;34m%s\e[m" "    Get the commit-msg bash script"
-
-    curl -fL -o "$LOCAL/commit-msg" "$REMOTE/commit-msg" --no-progress-meter
-    chmod +x "$LOCAL/commit-msg"
-
-    # Now, download each scripts from the REMOTE to the LOCAL folder
-    LOCAL_SCRIPTS="$LOCAL/commit-msg-scripts"
-    REMOTE_SCRIPTS="$REMOTE/commit-msg-scripts"
-
-    # Create the local folder
-    mkdir -p "$LOCAL_SCRIPTS"
-
-    printf "\n\e[1;34m%s\e[m" "    Download commit-msg scripts"
-
-    # Download scripts
-    for script in "${arrScripts[@]}"; do
-        printf "\n\e[1;36m%s\e[m" "        Download $script"
-
-        curl -fL -o "$LOCAL_SCRIPTS/$script" "$REMOTE_SCRIPTS/$script" --no-progress-meter
-        chmod +x "$LOCAL_SCRIPTS/$script"
-    done
+    installHook "commit-msg"
+    installScripts "commit-msg" "${arrScripts[@]}"
 }
 
 # http://patorjk.com/software/taag/#p=display&f=Big&t=Git%20hooks
@@ -87,13 +96,13 @@ EOF
 # curl -fL -o install.sh $REMOTE/install.sh
 
 # shellcheck disable=SC2034
-read -r -p "Do you want install hooks globally (Yes means for all repositories) or only for this repo? [Y/n]:" yesno
+read -r -p "Do you want install hooks for this repository (Y) or globally (n)? [Y/n]:" yesno
 
 # Transform to lowercase
 yesno="${yesno,,}"
 
-if [[ "$yesno" == "y" || "$yesno" == "" ]]; then
-    # Globally
+# No means globally
+if [[ "$yesno" == "n" ]]; then
     LOCAL="$(realpath ~)/.git/hooks"
     mkdir -p "$LOCAL"
 
